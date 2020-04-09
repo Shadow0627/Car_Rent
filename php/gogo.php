7796bf4f02bf30b65eb9a223a37ee73210d88d55
Shadow0627 <piorom12@gmail.com>
2020-04-09 13:37:08 +0200

    back car 99%

--- php/gogo.php
+++ php/gogo.php
@@ -8,5 +8,16 @@ $bak = $_SESSION['bak'];
 $km = $_SESSION['km'];
 $sql = "SELECT MAX(id_wynajmu) AS maxxx FROM wynajecie WHERE id_klient = '" .$klient_id. "' AND id_auto = '" .$auto. "'";
 include('db.php');
-
+$query = mysqli_query($conn, $sql);
+$done = mysqli_fetch_array($query);
+$id_wynajmu = $done['maxxx'];
+$data = date('y-m-d');
+$sql = "INSERT INTO zwrot (id_wynajmu, data_zwrotu, przebieg_zwrot, ilosc_paliwa, doplata) VALUES ('" .$id_wynajmu."', '" .$data."', '" .$km."', '" .$bak."', '" .$doplata."')";
+if(mysqli_query($conn, $sql))
+{
+    header('Location: /index.php');
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    die();
+}
 ?>
